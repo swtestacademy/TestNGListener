8a7dfbe31fb314b9ba5aa6594f16f37f4029ac4a
Onur Baskirt <onur_baskirt@yahoo.com>
2016-12-15 22:59:42 +0300

    annotations removed.

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -1,12 +1,6 @@
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.SkipException;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
-import static org.testng.Assert.assertEquals;
 
 
 /**
