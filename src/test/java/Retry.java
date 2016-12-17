ffd6cc968517c966c63f042d598bbed693cd0eb1
Onur Baskirt <onur_baskirt@yahoo.com>
2016-12-17 21:30:17 +0300

    Retry Logic
    TestNG Retry Logic

---
+++ src/test/java/Retry.java
@@ -0,0 +1,28 @@
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+/**
+ * Created by ONUR on 17.12.2016.
+ */
+public class Retry implements IRetryAnalyzer {
+
+    private int count = 0;
+    private static int maxTry = 3;
+
+    @Override
+    public boolean retry(ITestResult iTestResult) {
+        if (!iTestResult.isSuccess()) {                      //Check if test not succeed
+            if (count < maxTry) {                            //Check if maxtry count is reached
+                count++;                                     //Increase the maxTry count by 1
+                iTestResult.setStatus(ITestResult.FAILURE);  //Mark test as failed
+                return true;                                 //Tells TestNG to re-run the test
+            } else {
+                iTestResult.setStatus(ITestResult.FAILURE);  //If maxCount reached,test marked as failed
+            }
+        } else {
+            iTestResult.setStatus(ITestResult.SUCCESS);      //If test passes, TestNG marks it as passed
+        }
+        return false;
+    }
+
+}
--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -2,14 +2,29 @@ import org.testng.Assert;
 import org.testng.SkipException;
 import org.testng.annotations.Test;
 
+import static org.testng.Assert.assertEquals;
+
 
 /**
  * Created by ONUR on 11.12.2016.
  */
 public class SampleTest extends BaseTest{
 
-
+    //@Test(retryAnalyzer = Retry.class)
     @Test
+    public void test1() {
+        //Fail Scenario
+        assertEquals(2+2,5,"Addition Problem! 2+2 must be 4!\n");
+    }
+
+    //@Test(retryAnalyzer = Retry.class)
+    @Test
+    public void test2() {
+        //Fail Scenario
+        assertEquals(2+2,7,"Addition Problem! 2+2 must be 4!\n");
+    }
+
+/*    @Test
     public void test1() {
         System.out.println("I am in test1 test method and it will pass.");
     }
@@ -33,6 +48,6 @@ public class SampleTest extends BaseTest{
             System.out.println("test4 failed!");
             Assert.assertEquals(i, 8);
         }
-    }
+    }*/
 
 }
